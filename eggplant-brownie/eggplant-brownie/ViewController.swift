13a823fabc70f534daee96f0b8c99dad5c3139f8
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2022-07-14 00:05:34 -0300

    added button action

--- eggplant-brownie/eggplant-brownie/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func addHappy() {
+        print("test: --")
     }
 
 
